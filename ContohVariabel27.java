f2d9ca608d05536acb77f1aed18eac1b2d0defb3
sharlyf Syani <shirolyfsyani@gmail.com>
2023-10-04 13:08:08 +0700

    Tugas Jobsheet 3

---
+++ ContohVariabel27.java
@@ -0,0 +1,15 @@
+public class ContohVariabel27 {
+
+    public static void main(String[] args) {
+        String salahsatuhobbysayaadalah = "membaca buku";
+        boolean isPandai = true;
+        char jeniskelamin = 'L';
+        byte _umursayasekarang = 20;
+        double $ipk = 4.00, tinggi = 178;
+        System.out.println(salahsatuhobbysayaadalah);
+        System.out.println("apakah pandai? " + isPandai);
+        System.out.println("jenis kelamin: " + jeniskelamin);
+        System.out.println("umurku saat ini: " + _umursayasekarang);
+        System.out.println(String.format("saya beripk 4.00, dengan tinggi badan 178, $ipk, tinggi"));
+    }
+}
